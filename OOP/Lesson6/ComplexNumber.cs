9cdcbfdb38e0e7db48dbc638b9872c873dfab503
Linda Busra Berg <linda.busra.berg@gmail.com>
2025-04-30 23:36:23 +0200

    last

---
+++ OOP/Lesson6/ComplexNumber.cs
@@ -0,0 +1,18 @@
+public class ComplexNumber
+{
+    public double Real { get; set; }
+    public double Imaginary { get; set; }
+
+    public ComplexNumber(double real, double imaginary)
+    {
+        Real = real;
+        Imaginary = imaginary;
+    }
+
+    // Overloader + operatøren
+    public static ComplexNumber operator +(ComplexNumber c1, ComplexNumber c2)
+    {
+        return new ComplexNumber(c1.Real + c2.Real, c1.Imaginary + c2.Imaginary);
+    }
+}
+
