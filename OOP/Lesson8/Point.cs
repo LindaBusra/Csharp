f6133b2468ea1790dfc9aea0254dec0ee22bf36d
Linda Busra Berg <linda.busra.berg@gmail.com>
2025-05-12 23:52:34 +0200

    last

---
+++ OOP/Lesson8/Point.cs
@@ -0,0 +1,12 @@
+//OPERATORER OG HENDELSER I KLASSER
+
+//Operator Overloading
+public class Point{
+
+    public int X {get; set;}
+    public int Y {get; set;}
+
+    public static Point operator +(Point a, Point b){
+        return new Point { X = a.X + b.X, Y = a.Y + b.Y};
+    }
+}
