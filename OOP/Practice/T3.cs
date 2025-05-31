2a8ad3f4ce424e2f08d4a10edb3ac03641cfd975
Linda Busra Berg <linda.busra.berg@gmail.com>
2025-05-31 23:49:07 +0200

    last

---
+++ OOP/Practice/T3.cs
@@ -0,0 +1,46 @@
+
+/*
+Console.WriteLine("-----------String Concatenation-------------------------------------------------");
+string firstName = "John ";
+string lastName = "Doe";
+string name = firstName + lastName;
+Console.WriteLine(name);
+
+
+string fullname = string.Concat(firstName, lastName);
+Console.WriteLine(fullname);
+
+
+//String Interpolation
+string fullname2 = $"fullname is {firstName} {lastName}";
+Console.WriteLine(fullname2);
+
+//prints the second character (1) in myString:
+string myString = "Hello";
+Console.WriteLine(myString[1]);     //e
+
+//find the index position of a specific character in a string
+Console.WriteLine(myString.IndexOf("e"));       //1
+
+
+//use Substring() method to find lastname
+string studentFullname =  "John Nilsen";
+int charN = studentFullname.IndexOf("N");
+
+string studentLastname = studentFullname.Substring(charN);
+Console.WriteLine(studentLastname);
+
+
+//Strings - Special Characters  \'-->'      \"-->"     \\-->\
+
+string txt = "We are the so-called \"Vikings\" from the north.";
+Console.WriteLine(txt);
+
+
+txt = "It\'s alright.";
+Console.WriteLine(txt);
+
+txt = "The character \\ is called backslash.";
+Console.WriteLine(txt);
+
+*/
