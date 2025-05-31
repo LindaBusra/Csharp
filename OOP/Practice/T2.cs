0c7f108d1775b84bdcd9aac4ee3b3d4658f133fc
Linda Busra Berg <linda.busra.berg@gmail.com>
2025-05-31 23:48:41 +0200

    last

---
+++ OOP/Practice/T2.cs
@@ -0,0 +1,73 @@
+/*
+
+Console.WriteLine("-----------------ternary operator--------------------");
+int time = 20;
+string result = (time<18) ? "Good day" : "Good evening";
+Console.WriteLine(result);
+
+
+
+Console.WriteLine("-----------------switch--------------------");
+int day = 2;
+switch(day) {
+    case 1:
+    Console.WriteLine("Monday");
+    break;
+
+    case 2:
+    Console.WriteLine("Tuesday");
+    break;
+
+    default:
+    Console.WriteLine("Looking forward to the Weekend");
+    break;
+}
+
+
+Console.WriteLine("-----------------while--------------------");
+//Print j as long as j is less than 6.
+
+int j = 1;
+
+while(j < 6) {
+    Console.WriteLine(j);
+    j++;
+}
+
+
+//Use the do/while loop to print i as long as i is less than 6.
+int i = 1;
+do{
+    Console.WriteLine(i);
+    i++;
+} while(i<6);
+
+
+
+//Use a for loop to print "Yes" 5 times:
+for(int k=0; k<5; k++){
+    Console.WriteLine("Yes");
+}
+
+
+//Stop the loop if i is 5.
+for(int k=0; k<10; k++) {
+
+    if(k==5) {
+        break;
+    }
+    Console.WriteLine(k);
+}
+
+
+//In the following loop, when the value is "4", jump directly to the next value.
+for(int k =0; k<10; k++){
+
+    if(k==4){
+        continue;
+    }
+    Console.WriteLine(k);
+}
+
+
+*/
