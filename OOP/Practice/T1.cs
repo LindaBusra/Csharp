fe25065b5ab875e508dbc40644bfc80cf347a6e5
Linda Busra Berg <linda.busra.berg@gmail.com>
2025-05-31 23:48:17 +0200

    last

---
+++ OOP/Practice/T1.cs
@@ -0,0 +1,126 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+//dotnet new console -n VehicleRegister
+Console.WriteLine("Hello, World!");
+
+//Data types - Variables:
+string name="Ole";
+string lastname = "Jensen";
+int num=5;
+double d1 = 3.2d;
+double d2 = 5.68;
+char myLetter = 'D';
+bool isTrue = true;
+long myLong = 15000000000L;
+float myFloat = 5.75F;
+
+Console.WriteLine(name);
+Console.WriteLine(num);
+Console.WriteLine(d1);
+Console.WriteLine(d2);
+Console.WriteLine(myLetter);
+Console.WriteLine(isTrue);
+Console.WriteLine(myLong);
+Console.WriteLine(myFloat);
+Console.WriteLine(name + " " + lastname);
+
+//constant - unchangeable
+const int number = 23;
+//number = 80;
+Console.WriteLine(number);
+
+int myInt = 10;
+Console.WriteLine(Convert.ToString(myInt));
+
+
+Console.WriteLine("-------------Casting----------------------------------");
+//Implicit Casting (automatically) - converting a smaller type to a larger type size
+//int -> long -> float -> double  
+int myInteger = 10;
+double myDouble = myInteger;    // Automatic casting: int to double
+
+
+//Explicit Casting (manually) - converting a larger type to a smaller size type
+//double -> float -> long -> int
+double myDoubleNum = 8.9;
+int myIntNum = (int) myDoubleNum;   // Manual casting: double to int
+
+
+//Type Conversion Methods
+int i = 10;
+double d3 = 5.25;
+bool myBool = true;
+
+Console.WriteLine("--------------Type Conversion Methods------------------------------");
+Console.WriteLine(Convert.ToString(i));
+Console.WriteLine(Convert.ToDouble(i));
+Console.WriteLine(Convert.ToInt32(d3));
+Console.WriteLine(Convert.ToString(myBool));
+
+
+//Get User Input
+Console.WriteLine("--------------Get User Input----------------------------------");
+Console.WriteLine("What is your name?");
+string yourName = Console.ReadLine();
+Console.WriteLine("Hello " + yourName);
+Console.WriteLine("What is your age?");
+int age = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Your age is " + age);
+
+
+
+//Comparison Operators
+Console.WriteLine("--------------------Comparison Operators-----------------------------------------");
+int x = 20;
+int y = 35;
+Console.WriteLine(x>y);     //false
+
+
+//The Math.Max(x,y) method can be used to find the highest value of x and y:
+Console.WriteLine(Math.Max(45,89));
+
+//The Math.Min(x,y) method can be used to find the lowest value of of x and y:
+Console.WriteLine(Math.Min(45,78));
+
+//The Math.Sqrt(x) method returns the square root of x:
+Console.WriteLine(Math.Sqrt(64));
+
+//The Math.Abs(x) method returns the absolute (positive) value of x:
+Console.WriteLine(Math.Abs(-15));
+
+//Math.Round() rounds a number to the nearest whole number:
+Console.WriteLine(Math.Round(8.999));
+
+
+
+//concatenate two strings:
+string firstName = "John";
+string lastName = "Doe";
+string fullname = $"My full name is: {firstName} {lastName}";
+Console.WriteLine(fullname);
+Console.WriteLine(firstName[0]);        //J Access the first character
+
+
+//length of string
+string txt = "Hello World";
+Console.WriteLine(txt.Length);
+
+//string to upper case letters
+Console.WriteLine(txt.ToUpper());
+Console.WriteLine(txt.ToLower());
+
+int a = 47;
+int b = 35;
+
+if(a > b){
+    Console.WriteLine("Hello ... ");
+}
+
+
+
+
+
+
+
+*/
