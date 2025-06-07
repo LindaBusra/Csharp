f16109a72b69e2a85a09b2c56047a1228154a0ab
Linda Busra Berg <linda.busra.berg@gmail.com>
2025-06-08 00:51:01 +0200

    last

---
+++ OOP/VehicleRegister_uke6/interface/IVehicle.cs
@@ -0,0 +1,9 @@
+namespace VehicleRegister_uke6.interfaces;
+
+public interface IVehicle
+{
+    void Start();
+    void Stop();
+    string GetVehicleInfo();
+    // bool IsVehicleRegistered(Vehicle vehicle);
+}
