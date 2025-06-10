f1becc411e2e89d10db65d5bff8265fd8a609cc8
Linda Busra Berg <linda.busra.berg@gmail.com>
2025-06-11 00:07:04 +0200

    last

---
+++ OOP/VehicleRegister_uke6/Program.cs
@@ -0,0 +1,52 @@
+﻿
+using VehicleRegister_uke6.model;
+using VehicleRegister_uke6.service;
+
+var vehicleService = new VehicleService();
+
+while (true)
+{
+    Console.WriteLine("***************************");
+    Console.WriteLine("1. Legg til bil!");
+    Console.WriteLine("2. Legg til lastebil!");
+    Console.WriteLine("3. Legg til motorsykkel!");
+    Console.WriteLine("4. Legg til boat!");
+    Console.WriteLine("5. Vis alle kjøretøy!");
+    Console.WriteLine("6. Avslutt!");
+    Console.WriteLine("***************************");
+
+    Console.Write("Ditt valg: ");
+    string choice = Console.ReadLine();
+    
+    switch (choice)
+    {
+        case "1":
+            Console.WriteLine("Vennligst skriv inn informasjonen nedenfor for å registrere bilen");
+            vehicleService.AddVehicle(Car.CreateCar());
+            break;
+        case "2":
+            Console.WriteLine("Vennligst skriv inn informasjonen nedenfor for å registrere lastebilen");
+            vehicleService.AddVehicle(Truck.CreateTruck());
+            break;
+        case "3":
+            Console.WriteLine("Vennligst skriv inn informasjonen nedenfor for å registrere motorsykkelen");
+            vehicleService.AddVehicle(Motorcycle.CreateMotorcycle());
+            break;
+        case "4":
+            Console.WriteLine("Vennligst skriv inn informasjonen nedenfor for å registrere båten");
+            vehicleService.AddVehicle(Boat.CreateBoat());
+            break;
+        case "5":
+            vehicleService.ShowVehicles();
+            break;
+        case "6":
+            Console.WriteLine("--- BYE ---");
+            return;
+        default:
+            Console.WriteLine("Ugyldig valg, prøv igjen!");
+            break;
+    }
+}
+
+
+
