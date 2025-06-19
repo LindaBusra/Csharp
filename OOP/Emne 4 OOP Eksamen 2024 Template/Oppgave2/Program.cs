6a27c8f87b4eb491d5b13fed3145fc7faf49e378
Linda Busra Berg <linda.busra.berg@gmail.com>
2025-06-19 23:48:32 +0200

    last

---
+++ OOP/Emne 4 OOP Eksamen 2024 Template/Oppgave2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Oppgave 2
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Setter opp et nytt Bok-eksemplar med definerte egenskaper
+        Bok bok = new Bok("900-12-01-2024-1", "Bare et barn", "Sofia Strømsvold", 423);
+        Console.WriteLine("Ny bok opprettet:");
+        Console.WriteLine(bok.GetInfo());
+
+        // Legger til sider i boken
+        Console.WriteLine("\nLegger til 10 sider");
+        bok.IncreasePageCount(10);
+        Console.WriteLine(bok.GetInfo());
+
+        // Fjerner sider fra boken
+        Console.WriteLine("\nFjerner 60 sider");
+        bok.DecreasePageCount(60); 
+        Console.WriteLine(bok.GetInfo());
+    }
+}
